e7b1c1d6a7bda0feac4287dcf68ba8b717497a55
Victor Plushko <vicplushko33@gmail.com>
2020-04-09 04:36:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+  attr_accessor :name, :artist, :genre
+     @@count = 0
+     @@artists = []
+     @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+    end
+    
+    def self.count
+      @@count
+   end
+   
+   def self.artists
+     @@artists.uniq
+   end
+   
+   def self.genres
+     @@genres.uniq
+   end
+   
+   def self.genre_count
+     genre_hash = {}
+    @@genres.each {|genre| genre_hash[genre] +=1}
+    genre_hash
+    end
+ end
+
+   
